69832cce0ea6698750196c4c7663b2e217f0af0b
Allanroy <1848121547@qq.com>
2023-03-06 20:26:13 +0800

    +

--- page/index/index.js
+++ page/index/index.js
@@ -4,6 +4,16 @@ Page({
       'http://www.wtyz.vip/Nhimage/003.jpg',
       'http://www.wtyz.vip/Nhimage/004.jpg'
     ],
+    SelectedGoodsTop:[
+      {id:1,imgUrls:"http://www.wtyz.vip/Nhimage/toushi.jpg"},
+       {id:2,imgUrls:"http://www.wtyz.vip/Nhimage/zanzi.jpg"}
+    ],
+    SelectedGoodBottom:{id:1,imgUrls:"http://www.wtyz.vip/Nhimage/xiangbao.jpg"},
+    NewGoods:[
+      {id:1,imgUrls:"http://www.wtyz.vip/Nhimage/shanzi.jpg",name:"扇子",price:99.9}, {id:2,imgUrls:"http://www.wtyz.vip/Nhimage/bag.jpg",name:"包包",price:99.9},
+      {id:3,imgUrls:"http://www.wtyz.vip/Nhimage/ershi.jpg",name:"耳饰",price:99.9}, {id:4,imgUrls:"http://www.wtyz.vip/Nhimage/shuzi.jpg",name:"梳子",price:99.9},
+      {id:5,imgUrls:"http://www.wtyz.vip/Nhimage/toushu.jpg",name:"头梳",price:99.9}, {id:6,imgUrls:"http://www.wtyz.vip/Nhimage/xiangbao.jpg",name:"香包",price:99.9}
+    ],
     indicatorDots: false,
     autoplay: true,
     interval: 3000,
