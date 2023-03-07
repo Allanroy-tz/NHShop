0bcfb2d5fbe7f4f4f8e89a68af531b5e0112ce1c
Allanroy <1848121547@qq.com>
2023-03-07 16:22:56 +0800

    解决冲突

--- page/index/index.js
+++ page/index/index.js
@@ -4,21 +4,9 @@ Page({
       'http://www.wtyz.vip/Nhimage/003.jpg',
       'http://www.wtyz.vip/Nhimage/004.jpg'
     ],
-<<<<<<< HEAD
-    SelectedGoodsTop:[
-      {id:1,imgUrls:"http://www.wtyz.vip/Nhimage/toushi.jpg"},
-       {id:2,imgUrls:"http://www.wtyz.vip/Nhimage/zanzi.jpg"}
-    ],
-    SelectedGoodBottom:{id:1,imgUrls:"http://www.wtyz.vip/Nhimage/xiangbao.jpg"},
-    NewGoods:[
-      {id:1,imgUrls:"http://www.wtyz.vip/Nhimage/shanzi.jpg",name:"扇子",price:35}, {id:2,imgUrls:"http://www.wtyz.vip/Nhimage/bag.jpg",name:"包包",price:60},
-      {id:3,imgUrls:"http://www.wtyz.vip/Nhimage/ershi.jpg",name:"耳饰",price:20}, {id:4,imgUrls:"http://www.wtyz.vip/Nhimage/shuzi.jpg",name:"梳子",price:15},
-      {id:5,imgUrls:"http://www.wtyz.vip/Nhimage/toushu.jpg",name:"头梳",price:15}, {id:6,imgUrls:"http://www.wtyz.vip/Nhimage/xiangbao.jpg",name:"香包",price:30}
-=======
     SelectedGoodsTop:[ ],
     SelectedGoodBottom:{},
     NewGoods:[  
->>>>>>> bbb40fc8d1ffddbc06599d9456d696010ddfed6a
     ],
     indicatorDots: false,
     autoplay: true,
