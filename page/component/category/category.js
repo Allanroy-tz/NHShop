b0e23cc4cf969c3d7de78957afa96a7a6bfbc350
yutttz <3490005925@qq.com>
2023-03-07 21:03:36 +0800

    id添加

--- page/component/category/category.js
+++ page/component/category/category.js
@@ -11,17 +11,17 @@ Page({
         ],
         detail:[
           {id:'ornament',banner:'http://www.wtyz.vip/Nhimage/toushi.jpg',cate:'饰品',detail:[{thumb:'http://www.wtyz.vip/Nhimage/toushi.jpg',name:'头饰'},
-          {thumb:'http://www.wtyz.vip/Nhimage/toushu.jpg',name:'头梳'},{thumb:'http://www.wtyz.vip/Nhimage/shuzi.jpg',name:'梳子'},{thumb:'http://www.wtyz.vip/Nhimage/zanzi.jpg',name:'簪子'},{thumb:'http://www.wtyz.vip/Nhimage/ershi.jpg',name:'耳饰'},]},
+          {thumb:'http://www.wtyz.vip/Nhimage/toushu.jpg',name:'头梳',id:12},{thumb:'http://www.wtyz.vip/Nhimage/zanzi.jpg',name:'簪子',id:9},{thumb:'http://www.wtyz.vip/Nhimage/ershi.jpg',name:'耳饰',id:2}]},
 
-          {id:'kniting',banner:'http://www.wtyz.vip/Nhimage/bag.jpg',cate:'针织',detail:[{thumb:'http://www.wtyz.vip/Nhimage/bag.jpg',name:'包'},{thumb:'http://www.wtyz.vip/Nhimage/weijin.jpg',name:'围巾'}]},
+          {id:'kniting',banner:'http://www.wtyz.vip/Nhimage/bag.jpg',cate:'针织',detail:[{thumb:'http://www.wtyz.vip/Nhimage/bag.jpg',name:'包',id:1},{thumb:'http://www.wtyz.vip/Nhimage/weijin.jpg',name:'围巾',id:7},{thumb:'http://www.wtyz.vip/Nhimage/xiangbao.jpg',name:'香包',id:6}]},
 
-          {id:'bambooArt',banner:'http://www.wtyz.vip/Nhimage/kuaizi.jpg',cate:'竹艺',detail:[{thumb:'http://www.wtyz.vip/Nhimage/kuaizi.jpg',name:'筷子'}]},
+          {id:'bambooArt',banner:'http://www.wtyz.vip/Nhimage/kuaizi.jpg',cate:'竹艺',detail:[{thumb:'http://www.wtyz.vip/Nhimage/kuaizi.jpg',name:'筷子',id:4}]},
 
-          {id:'strawHat',banner:'http://www.wtyz.vip/Nhimage/maozi.jpg',cate:'草帽',detail:[{thumb:'http://www.wtyz.vip/Nhimage/maozi.jpg',name:'咸草帽'}]},
+          {id:'strawHat',banner:'http://www.wtyz.vip/Nhimage/maozi.jpg',cate:'草帽',detail:[{thumb:'http://www.wtyz.vip/Nhimage/maozi.jpg',name:'咸草帽',id:13}]},
 
-          {id:'paperCut',banner:'http://www.wtyz.vip/Nhimage/rabbit.jpg',cate:'剪纸',detail:[{thumb:'http://www.wtyz.vip/Nhimage/rabbit.jpg',name:'剪纸'}]},
+          {id:'paperCut',banner:'http://www.wtyz.vip/Nhimage/rabbit.jpg',cate:'剪纸',detail:[{thumb:'http://www.wtyz.vip/Nhimage/rabbit.jpg',name:'剪纸',id:3}]},
 
-          {id:'other',banner:'http://www.wtyz.vip/Nhimage/shoushi.jpg',cate:'其他',detail:[{thumb:'http://www.wtyz.vip/Nhimage/shoushi.jpg',name:'一套首饰'}]},
+          {id:'other',banner:'http://www.wtyz.vip/Nhimage/shoushi.jpg',cate:'其他',detail:[{thumb:'http://www.wtyz.vip/Nhimage/shoushi.jpg',name:'一套首饰',id:5},{thumb:'http://www.wtyz.vip/Nhimage/shuzi.jpg',name:'梳子',id:14}]},
         
         ],
         curIndex: 0,
